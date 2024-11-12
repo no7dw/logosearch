6fdfe82ea2ef3d00a8b257c13d63a11b8a388935
no7david <no7david@gmail.com>
2024-11-12 20:46:45 +0800

    change name

--- tests/test_logosearch.py
+++ tests/test_logosearch.py
@@ -1,10 +1,10 @@
 import pytest
 from src.logosearch.api import get_logo_url
 
-class TestLogoFinder:
+class TestLogoSearch:
     def test_get_logo_url_basic(self):
         """Test basic functionality of get_logo_url"""
-        logo_url = get_logo_url("elon musk", size="medium")
+        logo_url = get_logo_url("btc", size="medium")
         print(logo_url)
         assert isinstance(logo_url, str), "Logo URL should be a string"
         assert logo_url.startswith("http"), "Logo URL should be a valid URL"
